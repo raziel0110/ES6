227f421aaf49f6f0455c2f2f1019ae7bfcd81c63
Gabriel <albangabriel42@gmail.com>
2019-01-07 19:14:08 +0200

    first homework

---
+++ song.js
@@ -0,0 +1,12 @@
+var genre = "Rock";
+var duration = {
+    min : 3,
+    sec : 33
+};
+
+var artist = "Within Temtation";
+var name_song = "Faster";
+var band_members = ["Sharon den Adel","Robert Westerholt","Jeroen van Veen","Ruud Adrianus Jolie","Martijn Spierenburg","Mike Coolen","Stefan Helleblad"];
+
+console.log(name_song + " by "+ artist +" is "+ duration.min+":"+duration.sec+" long.");
+console.log("Members of "+ artist +" are: "+ band_members +" and they sing "+ genre);
